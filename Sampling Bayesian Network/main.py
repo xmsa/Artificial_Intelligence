9ab22be254eff4eee3ff090b74638483384e213f
Mohammad Mazaheri <msaxmsa@GMAIL.com>
2023-04-08 18:24:51 +0430

    change main file

--- Sampling Bayesian Network/main.py
+++ Sampling Bayesian Network/main.py
@@ -1,11 +1,26 @@
-import os 
+import os
 import sys
 import random
 import numpy as np
-import pandas as pd 
+import pandas as pd
+from network import Network, Node
+from query import Queries, Query
+from sampling import real_value, gibbs_sampling, likelihood_sampling, rejection_sampling, prioir_sampling
+
+
+def sample(network, queries):
+    for i, query in enumerate(queries):
+        rv = real_value(network, query)
+        ps = prioir_sampling()
+        rs = rejection_sampling()
+        ls = likelihood_sampling()
+        gs = gibbs_sampling()
+        print(i+1, rv, ps, rs, ls, gs)
 
 def main():
-    pass
+    filename = 'input.txt'
+    network, queries = Network.read_file(filename=filename)
+    sample(network, queries)
 
 if __name__ == '__main__':
     main()
